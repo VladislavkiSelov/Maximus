3b1f47ac3b0d68038cf9276dc7cc2010a029f256
[VladislavkiSelov] <[vladislavkiselev.2007@gmail.com]>
2023-06-02 19:38:40 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+module.exports = {
+ mode: 'development',
+ entry: './js/script.js',
+ output: {
+   filename: 'main.js',
+   path: path.resolve(__dirname, 'dist'),
+ },
+ module: {
+    rules: [
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
+    ]
+  }
+};
