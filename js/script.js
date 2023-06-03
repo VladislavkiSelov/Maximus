a351c8de77e0ed223498975032f2c35477f27e96
[VladislavkiSelov] <[vladislavkiselev.2007@gmail.com]>
2023-06-04 01:29:17 +0300

    finish main page

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 import '../scss/style.scss'
-console.log('Hello');
+const blockHeader = document.querySelector('header')
+const categoryList = document.querySelector('.list_category')
+const categoryListAllLi = Array.from(categoryList.querySelectorAll('li'))
+const btnMenu = document.querySelector('.humburger')
+const parentBlock = document.createElement('div')
+const btnClose = document.createElement('div')
+btnClose.classList.add('btn_close')
+parentBlock.classList.add('modal_menu_box')
+
+btnMenu.addEventListener('click', () => {
+    parentBlock.classList.remove('remove_modal_menu')
+    parentBlock.textContent = '';
+    categoryListAllLi.forEach((item) => {
+        const element = document.createElement('p');
+        element.textContent = item.textContent;
+        parentBlock.appendChild(element)
+    })
+    parentBlock.appendChild(btnClose)
+    blockHeader.appendChild(parentBlock)
+})
+btnClose.addEventListener('click', () => {
+    parentBlock.classList.add('remove_modal_menu')
+})
